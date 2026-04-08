74a9d86d52e0cbbd92a138788ba8e7ea6b68899a
Eksa Purnomo <kudeeksa@gmail.com>
2026-04-08 20:27:31 +0700

    feat: Tambah images brandr bertema CND Logistics beserta integrasi gambar 4k

--- data/brands.js
+++ data/brands.js
@@ -1,244 +1,76 @@
 export const brands = [
-  {
-    id: 1,
-    src: "/assets/images/brands/brand-01.svg",
-    alt: "Proline",
-  },
-  {
-    id: 2,
-    src: "/assets/images/brands/brand-02.svg",
-    alt: "Iceberg",
-  },
-  {
-    id: 3,
-    src: "/assets/images/brands/brand-03.svg",
-    alt: "PinPoint",
-  },
-  {
-    id: 4,
-    src: "/assets/images/brands/brand-04.svg",
-    alt: "Clues",
-  },
-  {
-    id: 5,
-    src: "/assets/images/brands/brand-05.svg",
-    alt: "Snowflake",
-  },
-  {
-    id: 6,
-    src: "/assets/images/brands/brand-06.svg",
-    alt: "Proline",
-  },
-  {
-    id: 7,
-    src: "/assets/images/brands/brand-01.svg",
-    alt: "Proline",
-  },
-  {
-    id: 8,
-    src: "/assets/images/brands/brand-02.svg",
-    alt: "Iceberg",
-  },
-  {
-    id: 9,
-    src: "/assets/images/brands/brand-03.svg",
-    alt: "PinPoint",
-  },
-  {
-    id: 10,
-    src: "/assets/images/brands/brand-04.svg",
-    alt: "Clues",
-  },
-  {
-    id: 11,
-    src: "/assets/images/brands/brand-05.svg",
-    alt: "Snowflake",
-  },
-  {
-    id: 12,
-    src: "/assets/images/brands/brand-06.svg",
-    alt: "Proline",
-  },
+  { id: 1, src: "/assets/images/brands/tanto-logo.png", alt: "Tanto" },
+  { id: 2, src: "/assets/images/brands/Logo-spil.png", alt: "SPIL" },
+  { id: 3, src: "/assets/images/brands/temas-logo.jpg", alt: "Temas" },
+  { id: 4, src: "/assets/images/brands/logo-meratus.png", alt: "Meratus" },
+  { id: 5, src: "/assets/images/brands/Logo_Samudera_Indonesia_PT.png", alt: "Samudera Indonesia" },
+  { id: 6, src: "/assets/images/brands/tanto-logo.png", alt: "Tanto" },
+  { id: 7, src: "/assets/images/brands/Logo-spil.png", alt: "SPIL" },
+  { id: 8, src: "/assets/images/brands/temas-logo.jpg", alt: "Temas" },
+  { id: 9, src: "/assets/images/brands/logo-meratus.png", alt: "Meratus" },
+  { id: 10, src: "/assets/images/brands/Logo_Samudera_Indonesia_PT.png", alt: "Samudera Indonesia" },
+  { id: 11, src: "/assets/images/brands/tanto-logo.png", alt: "Tanto" },
+  { id: 12, src: "/assets/images/brands/Logo-spil.png", alt: "SPIL" },
 ];
 
 export const reviewLogos = [
-  {
-    id: 13,
-    lightSrc: "/assets/images/review-logo/01.svg",
-    darkSrc: "/assets/images/review-logo/dark-01.svg",
-  },
-  {
-    id: 14,
-    lightSrc: "/assets/images/review-logo/02.svg",
-    darkSrc: "/assets/images/review-logo/dark-02.svg",
-  },
-  {
-    id: 15,
-    lightSrc: "/assets/images/review-logo/03.svg",
-    darkSrc: "/assets/images/review-logo/dark-03.svg",
-  },
-  {
-    id: 16,
-    lightSrc: "/assets/images/review-logo/04.svg",
-    darkSrc: "/assets/images/review-logo/dark-04.svg",
-  },
-  {
-    id: 17,
-    lightSrc: "/assets/images/review-logo/05.svg",
-    darkSrc: "/assets/images/review-logo/dark-05.svg",
-  },
-  {
-    id: 18,
-    lightSrc: "/assets/images/review-logo/06.svg",
-    darkSrc: "/assets/images/review-logo/dark-06.svg",
-  },
+  { id: 13, lightSrc: "/assets/images/review-logo/01.svg", darkSrc: "/assets/images/review-logo/dark-01.svg" },
+  { id: 14, lightSrc: "/assets/images/review-logo/02.svg", darkSrc: "/assets/images/review-logo/dark-02.svg" },
+  { id: 15, lightSrc: "/assets/images/review-logo/03.svg", darkSrc: "/assets/images/review-logo/dark-03.svg" },
+  { id: 16, lightSrc: "/assets/images/review-logo/04.svg", darkSrc: "/assets/images/review-logo/dark-04.svg" },
+  { id: 17, lightSrc: "/assets/images/review-logo/05.svg", darkSrc: "/assets/images/review-logo/dark-05.svg" },
+  { id: 18, lightSrc: "/assets/images/review-logo/06.svg", darkSrc: "/assets/images/review-logo/dark-06.svg" },
 ];
 
 export const brands2 = [
-  {
-    id: 19,
-    src: "/assets/images/brands/brand-01.svg",
-    alt: "Proline",
-  },
-  {
-    id: 20,
-    src: "/assets/images/brands/brand-02.svg",
-    alt: "Iceberg",
-  },
-  {
-    id: 21,
-    src: "/assets/images/brands/brand-03.svg",
-    alt: "PinPoint",
-  },
-  {
-    id: 22,
-    src: "/assets/images/brands/brand-04.svg",
-    alt: "Clues",
-  },
-  {
-    id: 23,
-    src: "/assets/images/brands/brand-05.svg",
-    alt: "Snowflake",
-  },
-  {
-    id: 24,
-    src: "/assets/images/brands/brand-06.svg",
-    alt: "Proline",
-  },
-  {
-    id: 25,
-    src: "/assets/images/brands/brand-07.svg",
-    alt: "Iceberg",
-  },
-  {
-    id: 26,
-    src: "/assets/images/brands/brand-08.svg",
-    alt: "PinPoint",
-  },
+  { id: 19, src: "/assets/images/brands/tanto-logo.png", alt: "Tanto" },
+  { id: 20, src: "/assets/images/brands/Logo-spil.png", alt: "SPIL" },
+  { id: 21, src: "/assets/images/brands/temas-logo.jpg", alt: "Temas" },
+  { id: 22, src: "/assets/images/brands/logo-meratus.png", alt: "Meratus" },
+  { id: 23, src: "/assets/images/brands/Logo_Samudera_Indonesia_PT.png", alt: "Samudera Indonesia" },
+  { id: 24, src: "/assets/images/brands/tanto-logo.png", alt: "Tanto" },
+  { id: 25, src: "/assets/images/brands/Logo-spil.png", alt: "SPIL" },
+  { id: 26, src: "/assets/images/brands/temas-logo.jpg", alt: "Temas" },
 ];
 
 export const brands3 = [
-  {
-    id: 27,
-    src: "/assets/images/review-logo/01.svg",
-    darkSrc: "/assets/images/review-logo/dark-01.svg",
-  },
-  {
-    id: 28,
-    src: "/assets/images/review-logo/02.svg",
-    darkSrc: "/assets/images/review-logo/dark-02.svg",
-  },
-  {
-    id: 29,
-    src: "/assets/images/review-logo/03.svg",
-    darkSrc: "/assets/images/review-logo/dark-03.svg",
-  },
-  {
-    id: 30,
-    src: "/assets/images/review-logo/04.svg",
-    darkSrc: "/assets/images/review-logo/dark-04.svg",
-  },
-  {
-    id: 31,
-    src: "/assets/images/review-logo/05.svg",
-    darkSrc: "/assets/images/review-logo/dark-05.svg",
-  },
-  {
-    id: 32,
-    src: "/assets/images/review-logo/06.svg",
-    darkSrc: "/assets/images/review-logo/dark-06.svg",
-  },
+  { id: 27, src: "/assets/images/brands/tanto-logo.png", darkSrc: "/assets/images/brands/tanto-logo.png" },
+  { id: 28, src: "/assets/images/brands/Logo-spil.png", darkSrc: "/assets/images/brands/Logo-spil.png" },
+  { id: 29, src: "/assets/images/brands/temas-logo.jpg", darkSrc: "/assets/images/brands/temas-logo.jpg" },
+  { id: 30, src: "/assets/images/brands/logo-meratus.png", darkSrc: "/assets/images/brands/logo-meratus.png" },
+  { id: 31, src: "/assets/images/brands/Logo_Samudera_Indonesia_PT.png", darkSrc: "/assets/images/brands/Logo_Samudera_Indonesia_PT.png" },
+  { id: 32, src: "/assets/images/brands/tanto-logo.png", darkSrc: "/assets/images/brands/tanto-logo.png" },
 ];
 
 export const brands4 = [
-  { alt: "Proline", src: "/assets/images/brands/brand-01.svg", width: 165 },
-  { alt: "Iceberg", src: "/assets/images/brands/brand-02.svg", width: 146 },
-  { alt: "PinPoint", src: "/assets/images/brands/brand-03.svg", width: 178 },
-  { alt: "Clues", src: "/assets/images/brands/brand-04.svg", width: 177 },
-  { alt: "Snowflake", src: "/assets/images/brands/brand-05.svg", width: 179 },
-  { alt: "Proline", src: "/assets/images/brands/brand-06.svg", width: 167 },
-  { alt: "Iceberg", src: "/assets/images/brands/brand-07.svg", width: 170 },
-  { alt: "PinPoint", src: "/assets/images/brands/brand-08.svg", width: 163 },
+  { alt: "Tanto", src: "/assets/images/brands/tanto-logo.png", width: 165 },
+  { alt: "SPIL", src: "/assets/images/brands/Logo-spil.png", width: 146 },
+  { alt: "Temas", src: "/assets/images/brands/temas-logo.jpg", width: 178 },
+  { alt: "Meratus", src: "/assets/images/brands/logo-meratus.png", width: 177 },
+  { alt: "Samudera Indonesia", src: "/assets/images/brands/Logo_Samudera_Indonesia_PT.png", width: 179 },
+  { alt: "Tanto", src: "/assets/images/brands/tanto-logo.png", width: 167 },
+  { alt: "SPIL", src: "/assets/images/brands/Logo-spil.png", width: 170 },
+  { alt: "Temas", src: "/assets/images/brands/temas-logo.jpg", width: 163 },
 ];
 
 export const brands5 = [
-  { src: "/assets/images/apps/asana.svg", width: 2500, height: 2311 },
-  { src: "/assets/images/apps/mailchimp.svg", width: 351, height: 373 },
-  { src: "/assets/images/apps/zapier.svg", width: 800, height: 800 },
-  { src: "/assets/images/apps/bitbucket.svg", width: 333, height: 300 },
-  { src: "/assets/images/apps/drive.svg", width: 800, height: 800 },
+  { src: "/assets/images/brands/tanto-logo.png", width: 2500, height: 2311 },
+  { src: "/assets/images/brands/Logo-spil.png", width: 351, height: 373 },
+  { src: "/assets/images/brands/temas-logo.jpg", width: 800, height: 800 },
+  { src: "/assets/images/brands/logo-meratus.png", width: 333, height: 300 },
+  { src: "/assets/images/brands/Logo_Samudera_Indonesia_PT.png", width: 800, height: 800 },
 ];
 
 export const brands6 = [
-  {
-    src: "/assets/images/brands/brand-01.svg",
-    alt: "brand",
-    width: 165,
-    height: 48,
-  },
-  {
-    src: "/assets/images/brands/brand-05.svg",
-    alt: "brand",
-    width: 179,
-    height: 48,
-  },
-  {
-    src: "/assets/images/brands/brand-03.svg",
-    alt: "brand",
-    width: 178,
-    height: 48,
-  },
-  {
-    src: "/assets/images/brands/brand-07.svg",
-    alt: "brand",
-    width: 170,
-    height: 48,
-  },
+  { src: "/assets/images/brands/tanto-logo.png", alt: "Tanto", width: 165, height: 48 },
+  { src: "/assets/images/brands/Logo-spil.png", alt: "SPIL", width: 179, height: 48 },
+  { src: "/assets/images/brands/temas-logo.jpg", alt: "Temas", width: 178, height: 48 },
+  { src: "/assets/images/brands/logo-meratus.png", alt: "Meratus", width: 170, height: 48 },
 ];
 
 export const brands7 = [
-  {
-    src: "/assets/images/brands/brand-01.svg",
-    alt: "brand",
-    width: 165,
-    height: 48,
-  },
-  {
-    src: "/assets/images/brands/brand-07.svg",
-    alt: "brand",
-    width: 170,
-    height: 48,
-  },
-  {
-    src: "/assets/images/brands/brand-03.svg",
-    alt: "brand",
-    width: 178,
-    height: 48,
-  },
-  {
-    src: "/assets/images/brands/brand-05.svg",
-    alt: "brand",
-    width: 179,
-    height: 48,
-  },
+  { src: "/assets/images/brands/tanto-logo.png", alt: "Tanto", width: 165, height: 48 },
+  { src: "/assets/images/brands/Logo-spil.png", alt: "SPIL", width: 170, height: 48 },
+  { src: "/assets/images/brands/temas-logo.jpg", alt: "Temas", width: 178, height: 48 },
+  { src: "/assets/images/brands/logo-meratus.png", alt: "Meratus", width: 179, height: 48 },
 ];
